1270dfc4178b717f149fe1c6605eab6630cfc2e4
Parker Phinney <parker.phinney@gmail.com>
2009-12-21 01:00:31 -0800

    renamed scraper to menu-scraper, and added some comments.

--- menu-scraper.py
+++ menu-scraper.py
@@ -1,9 +1,11 @@
 import urllib
 import datetime
 
+# where we will keep the scraped html menu pages
 SCRAPE_DEST="/home/pyrak/scrapes/"
 
 def main():
+	#the url of the dailymenus page is based on the day of the week
 	now = datetime.datetime.now()	
 	weekdayDec = int(now.strftime("%w")) #[0,6]
 	#for them, mon = 1, sun = 7
@@ -11,19 +13,20 @@ def main():
 	#so we adjust
 	if (weekdayDec == 0):
 		menuPageName = "7. sunday.htm"	
-	if (weekdayDec == 1):
+	elif (weekdayDec == 1):
 		menuPageName = "2. Tuesday.htm"
-	if (weekdayDec == 2):
+	elif (weekdayDec == 2):
 		menuPageName = "3. Wednesday.htm"	
-	if (weekdayDec == 3):
+	elif (weekdayDec == 3):
 		menuPageName = "4. thursday.htm"	
-	if (weekdayDec == 4):
+	elif (weekdayDec == 4):
 		menuPageName = "5. friday.htm"	
-	if (weekdayDec == 5):
+	elif (weekdayDec == 5):
 		menuPageName = "6. saturday.htm"	
-	if (weekdayDec == 6):
+	elif (weekdayDec == 6):
 		menuPageName = "1. Monday.htm"	
 	
+	# download the html page with the daily menus
 	urllib.urlretrieve('http://www.dartmouth.edu/~dds/' + menuPageName, SCRAPE_DEST + '/' + str(now))
 
 if __name__ == '__main__':
